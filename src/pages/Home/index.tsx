03ebdc8d1a1bb9bed561d74661a91c8304f4451e
adebudev <adebudev@gmail.com>
2026-01-31 20:11:38 -0500

    feat: implement routing with Home and NotFound pages, add react-router-dom dependency

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,23 @@
+import Hero from '../../components/Hero';
+import Services from '../../components/Services';
+import Mission from '../../components/Mission';
+import Audience from '../../components/Audience';
+import Procedure from '../../components/Procedure';
+import Evidence from '../../components/Evidence';
+import Results from '../../components/Results';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Services />
+      <Mission />
+      <Audience />
+      <Procedure />
+      <Evidence />
+      <Results />
+    </>
+  );
+};
+
+export default Home;
